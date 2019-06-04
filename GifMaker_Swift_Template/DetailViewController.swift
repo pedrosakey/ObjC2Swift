06dd2e11bba87af496ade538a388b8efb18f10d5
Pedro Sánchez <pedro.sanchez.it@gmail.com>
2019-06-04 14:22:28 +0100

    [Add] SHARE” button has a pink border and rounded corners.

--- GifMaker_Swift_Template/DetailViewController.swift
+++ GifMaker_Swift_Template/DetailViewController.swift
@@ -13,10 +13,17 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var gifImageView: UIImageView!
     var gif: Gif?
     
+    @IBOutlet weak var shareGifButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         gifImageView.image = gif?.gifImageWithCaption
+        
+        // Buttom Set up
+        shareGifButton.layer.cornerRadius = 5
+        shareGifButton.layer.borderWidth = 1
+        shareGifButton.layer.borderColor = UIColor.magenta.cgColor
+        
     }
     
 
